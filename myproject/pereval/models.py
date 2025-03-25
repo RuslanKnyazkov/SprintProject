b017c71a134cf38c48d42f601314f2a16c054bbb
Ruslan <rus.knyazkov.94@mail.ru>
2025-03-25 11:48:18 +0300

    Create models in pereval app.

--- myproject/pereval/models.py
+++ myproject/pereval/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    user = models.OneToOneField(to=User, max_length=255,
+                                verbose_name='Пользователь',
+                                on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    phone = models.CharField(max_length=20, blank=True, unique=True)
+
+
+class Coords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+    def __str__(self):
+        return f"Lat: {self.latitude}, Lon: {self.longitude}, Height: {self.height}"
+
+class PerevalAdded(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+
+    beautyTitle = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.CharField(max_length=255, blank=True, null=True)
+    connect = models.TextField(blank=True, null=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    coord = models.ForeignKey(Coords, on_delete=models.CASCADE)
+    level_winter = models.CharField(max_length=50, blank=True, null=True)
+    level_spring = models.CharField(max_length=50, blank=True, null=True)
+    level_summer = models.CharField(max_length=50, blank=True, null=True)
+    level_autumn = models.CharField(max_length=50, blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
+
+    def __str__(self):
+        return self.title
+
+class PerevalImages(models.Model):
+    pereval = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE)
+    image = models.BinaryField()  # Для хранения бинарных данных изображения
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.pereval.title}"
