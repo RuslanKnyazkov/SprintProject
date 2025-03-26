044f4ed4d214d4551387e40451a75bb7d2aa4ac3
Ruslan <rus.knyazkov.94@mail.ru>
2025-03-26 15:46:06 +0300

    Add view PerevalDetailView with method get

--- myproject/pereval/urls.py
+++ myproject/pereval/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PerevalViewSet
+from .views import PerevalViewSet, PerevalDetailView
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = DefaultRouter()
 router.register(r'submitData', PerevalViewSet, basename='pereval')
 
 urlpatterns = [
+    path('submitData/<int:pk>/', PerevalDetailView.as_view(), name='pereval-detail'),
     path('', include(router.urls)),
+
 ]
+
--- myproject/pereval/views.py
+++ myproject/pereval/views.py
@@ -1,9 +1,35 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 from .models import Pereval
 from .serializers import PerevalSerializer
 from rest_framework.parsers import MultiPartParser, JSONParser
+
+class PerevalDetailView(APIView):
+    http_method_names = ['get']
+
+    def get_object(self, pk):
+        try:
+            return Pereval.objects.get(pk = pk)
+        except Exception as e:
+            return Response(status= status.HTTP_404_NOT_FOUND)
+    def get(self, request, pk, format=None):
+        try:
+            pereval = self.get_object(pk)
+            serializer = PerevalSerializer(pereval)
+            print(pereval, serializer)
+            return Response(serializer.data)
+        except Pereval.DoesNotExist:
+            return Response(
+                {"error": "Перевал с указанным ID не найден"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
 
 class PerevalViewSet(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
