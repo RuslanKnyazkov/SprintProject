4aed1c50efa42e0451c30988c89f2f6ce12326c5
Ruslan <rus.knyazkov.94@mail.ru>
2025-04-03 18:09:09 +0300

    Create test in pereval/tests

--- myproject/pereval/tests.py
+++ myproject/pereval/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import User, Coords, Pereval
+
+
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(name='Иван',
+                            fam='Иванов',
+                            otc='Иванович',
+                            email='ivanov@mail.ru',
+                            phone=89030332677)
+
+        coord = Coords.objects.create(latitude =1.45,
+                              longitude = 2.66,
+                              height = 5000)
+
+        Pereval.objects.create(beauty_title='Красивый горный перевал',
+                               title="Горный хребет",
+                               user=user,
+                               coords = coord)
+
+
+    def test_check_user_model(self):
+        user = User.objects.get(name = 'Иван')
+        self.assertEqual([user.name, user.fam, user.otc, user.email, user.phone] ,
+                         ['Иван', 'Иванов', 'Иванович', 'ivanov@mail.ru', '89030332677'])
+        self.assertEqual(type(user.phone), str)
+
+    def test_check_coord_model(self):
+        coord = Coords.objects.get(height = 5000)
+        self.assertEqual(coord.height, 5000)
+
+
+    def test_check_pereval_model(self):
+        pereval = Pereval.objects.get(user__name = 'Иван')
+        self.assertEqual(pereval.status, 'new')
+        pereval.status = 'rejected'
+        self.assertEqual(pereval.status, 'rejected')
+
+
+class ApiClientTestCase(TestCase):
+
+    def setUp(self):
+        self.list_url = reverse('pereval-list')
+
+    def test_client_method_get(self):
+        responce = self.client.get(self.list_url)
+        self.assertEqual(responce.status_code, 200)
